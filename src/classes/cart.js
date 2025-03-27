ebd573b8c731a935033054a5ad555016959e7af8
Nugget1368 <rneg1@hotmail.com>
2025-03-27 15:26:54 +0100

    Cart class w. get and set methods

---
+++ src/classes/cart.js
@@ -0,0 +1,17 @@
+export class Cart {
+    constructor(items = []) {
+        this.items = items;
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    getTotal() {
+        return this.items.reduce((total, item) => total + item.price, 0);
+    }
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import { fetchProducts, addProduct } from "../utils/api.js";
 import { Product } from "../classes/product.js";
+import { Cart } from "../classes/cart.js";
 
 document.addEventListener("DOMContentLoaded", loadProducts);
+let cart = new Cart();
 // Function to fetch and render products
 async function loadProducts() {
   const productsContainer = document.getElementById("products");
@@ -14,6 +16,19 @@ async function loadProducts() {
     if (products.length > 0) {
       products.forEach((product) => {
         const productCard = createProductCard(product);
+        //DELETE THIS
+        productCard.id = product.id;
+        productCard.querySelector(".add-to-cart-btn").id = product.id;
+        let btn = productCard.querySelector(`.add-to-cart-btn`);
+        btn.addEventListener("click", () => {
+            // alert(`Adding ${product.name + btn.id} to cart\nFunctionality not implemented yet`);
+            cart.addItem(product);
+            let items = cart.getItems();
+            let cartLink = document.querySelector("[data-cart]");
+            cartLink.textContent = `Cart (${items.length})`;
+            console.log(items);
+            console.log(`Total price: ${cart.getTotal()}`);
+          });
         productsContainer.appendChild(productCard);
       });
     } else {
@@ -36,9 +51,9 @@ function createProductCard(product) {
     <button class="add-to-cart-btn">Add to Cart</button>
   `;
 
-  element.querySelector(".add-to-cart-btn").addEventListener("click", () => {
-    alert(`Adding ${product.name} to cart\nFunctionality not implemented yet`);
-  });
+  // element.querySelector(".add-to-cart-btn").addEventListener("click", () => {
+  //   alert(`Adding ${product.name} to cart\nFunctionality not implemented yet`);
+  // });
 
   return element;
 }
