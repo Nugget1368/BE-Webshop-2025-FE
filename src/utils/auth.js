02b46a36af5efb5c751f1dd87ce2232b0b169466
NathalieBruno <144827786+NathalieBruno@users.noreply.github.com>
2025-04-08 12:29:54 +0200

    changed error message during registration and added help text to input fields

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,35 +1,34 @@
 const TOKEN_KEY = "token";
 
 export class auth {
-    static async register(user) {
-        try {
-            let response = await axios.post("https://webshop-2025-be-g4.vercel.app/api/auth/register", user);
-            return response;
-        }
-        catch (err) {
-            return err.message;
-        }
+  static async register(user) {
+    try {
+      let response = await axios.post("https://webshop-2025-be-g4.vercel.app/api/auth/register", user);
+      return response;
+    } catch (err) {
+      console.log("Feldetaljer:", err.response);
+      return err.response; // Returnera response-objektet från felet
     }
+  }
 
-    static async login(username, password) {
-        try {
-            let user = {
-                email: username,
-                password: password
-            }
-            let response = await axios.post("https://webshop-2025-be-g4.vercel.app/api/auth/login", user);
-            return response;
-        }
-        catch (err) {
-            return err;
-        }
+  static async login(username, password) {
+    try {
+      let user = {
+        email: username,
+        password: password,
+      };
+      let response = await axios.post("https://webshop-2025-be-g4.vercel.app/api/auth/login", user);
+      return response;
+    } catch (err) {
+      return err;
     }
+  }
 
-    static saveToken(token) {
-        sessionStorage.setItem(TOKEN_KEY, token);
-    }
+  static saveToken(token) {
+    sessionStorage.setItem(TOKEN_KEY, token);
+  }
 
-    static getToken() {
-        return sessionStorage.getItem(TOKEN_KEY);
-    }
+  static getToken() {
+    return sessionStorage.getItem(TOKEN_KEY);
+  }
 }
